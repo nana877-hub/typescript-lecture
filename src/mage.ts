0e60c8da96286b78e1e97fef68e5c8d6da80f75e
nana877-hub <nunaru.n877@gmail.com>
2026-06-30 21:58:36 +0900

    mageにattackメソッド、powerを追加

--- src/mage.ts
+++ src/mage.ts
@@ -1,7 +1,15 @@
-import { Character } from './character.ts';
+import { Character } from "./character.ts";
 
 export class Mage extends Character {
-  override attack() {
+  private power: number;
+
+  constructor(name: string, hp: number) {
+    super(name, hp); // super() <- 継承元 (character) の constructor
+    this.power = this.power;
+  }
+
+  attack(opponent: Character) {
     console.log(`${this.name}は魔法を唱えた！`);
+    opponent.takeDamage(this.power);
   }
 }
